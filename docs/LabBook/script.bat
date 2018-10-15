5212e85c809fd2f829c511b5f0096d9c52cd7a31
ivangarrera <iesivangarcia@gmail.com>
2018-10-15 18:07:56 +0200

    15October LabBook

---
+++ docs/LabBook/script.bat
@@ -0,0 +1,7 @@
+pdflatex -synctex=1 -interaction=nonstopmode main.tex > NUL
+
+pdflatex -synctex=1 -interaction=nonstopmode main.tex > NUL
+
+DEL main.aux main.synctex.gz main.toc main.log main.blg main.out main.bbl
+
+explorer.exe "main.pdf"
---
+++ docs/LabBook/script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+pdflatex -synctex=1 -interaction=nonstopmode main.tex 1,2> /dev/null
+
+pdflatex -synctex=1 -interaction=nonstopmode main.tex 1,2> /dev/null
+
+rm main.aux main.log main.synctex.gz main.toc main.blg main.out main.bbl
+
+evince main.pdf
