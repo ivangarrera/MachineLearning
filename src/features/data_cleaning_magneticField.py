106f65bac93c592421889dca271d8008628c7702
didimoj <didimoj@hotmail.com>
2018-10-23 17:27:49 +0200

    First steps with DBSCAN

--- src/features/data_cleaning_magneticField.py
+++ src/features/data_cleaning_magneticField.py
@@ -1,10 +1,11 @@
 from basic_data_cleaning import BasicCleaning
 
 
-data_set = BasicCleaning.CleanData(path_to_data=r'C:\Users\ivangarrera\Desktop\T2.csv')
+data_set = BasicCleaning.CleanData(path_to_data=r'D:\Ing. Informatica\Cuarto\Machine Learning\T2.csv')
 
 data_set = data_set[[column for column in data_set if \
                           "MagneticField" in column and column.endswith("MEAN")]]
 
-data_set.to_csv(r'C:\Users\ivangarrera\Desktop\T2_cleaned.csv',
+data_set.to_csv('D:\Ing. Informatica\Cuarto\Machine Learning\T2_cleaned_magneticField.csv', 
                 sep=',', index=False)
+
