028842c0fc395160ba40b62101bb5a83635cf991
ivangarrera <iesivangarcia@gmail.com>
2018-10-23 17:26:01 +0200

    Code refactored. Better use of KNearestNeighbors + AgglomerativeClustering algorithms

--- src/visualization/common_clustering.py
+++ src/visualization/common_clustering.py
@@ -2,8 +2,9 @@
 
 from sklearn import preprocessing
 from sklearn.decomposition import PCA
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, AgglomerativeClustering, DBSCAN
 from sklearn import metrics
+from sklearn.neighbors import kneighbors_graph
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -14,27 +15,23 @@ class CommonClustering:
         self.reduced_data = None
         self.silhouettes = []
     
-    def PrincipalComponentAnalysis(self, num_components):
+    def PrincipalComponentAnalysis(self, num_components, plot_graph=True):
         scaler = preprocessing.MinMaxScaler()
         data_norm = scaler.fit_transform(self.data_set)
         
         self.reduced_data = PCA(n_components=num_components).fit_transform(data_norm)
         
+        if plot_graph:
+            plt.scatter(self.reduced_data[:,0], self.reduced_data[:,1])
+            plt.show()
         
-    def PlotPCAResult(self):
-        x = self.reduced_data[:,0]
-        y = self.reduced_data[:,1]
-        plt.scatter(x,y)
-        plt.show()
 
     def getBestNumberOfClusters(self):
-        """
-            -------------- Silhuette cofficient -----------------
-            Best value is 1 and worst value is -1
-            Value near 0 indicates overlapping clusters
-            Negative value indicates that a sample has been asigned to the wrong cluster
-            Value near 1 indicates that the object is well matched with its cluster    
-        """
+        #    -------------- Silhuette cofficient -----------------
+        #    Best value is 1 and worst value is -1
+        #    Value near 0 indicates overlapping clusters
+        #    Negative value indicates that a sample has been asigned to the wrong cluster
+        #    Value near 1 indicates that the object is well matched with its cluster    
                 
         # Find the number of clusters that gives the best silhouettes coefficient
         for clus in range(2, 50):
@@ -48,16 +45,48 @@ class CommonClustering:
         print (max(self.silhouettes),'- Cluster:', ideal_number_of_clusters)
         return ideal_number_of_clusters
     
-    def KMeansWithIdeal(self, number_of_clusters):
+    def KMeansWithIdeal(self, number_of_clusters, plot_graph=True):
         k_means = KMeans(n_clusters=number_of_clusters, init="random", random_state=0)
         labels = k_means.fit_predict(self.reduced_data)
         
-        x = self.reduced_data[:,0]
-        y = self.reduced_data[:,1]
-        plt.scatter(x, y, c=labels)
-        # Plotting centroids
-        plt.scatter(k_means.cluster_centers_[:,0], k_means.cluster_centers_[:,1], c='red',s=50)
-        plt.show()
+        if plot_graph:
+            plt.scatter(self.reduced_data[:,0], self.reduced_data[:,1], c=labels)
+            # Plotting centroids
+            plt.scatter(k_means.cluster_centers_[:,0], k_means.cluster_centers_[:,1],
+                        c='red', s=50)
+            plt.show()
+    
+    def AgglomerativeClusteringWithNearestNeighbors(self, plot_graph=True, nneighbors=3, nclusters=5):
+        # Clustering (Agglomerative Clustering)
+        # KNN gives a connectivity matrix which will be used by Agglomerative Clustering. 
+        # This clustering algorithm will create a hierarchy with all points connected the ones
+        # with the others, so all points in the connectivity matrix must be connected.
+        # knn_graph returns a matrix with elements connected. Element a is connected to 
+        # element b if matrix[a][b] = 1. 
+        
+        knn_graph = kneighbors_graph(self.reduced_data, n_neighbors=nneighbors, include_self=False)
+        
+        
+        labels =  AgglomerativeClustering(n_clusters=nclusters, connectivity=knn_graph, 
+                                          linkage="complete").fit_predict(self.reduced_data)
+        
+        if plot_graph:
+            plt.scatter(self.reduced_data[:,0], self.reduced_data[:,1], c=labels)
+            plt.show()
+    
+    def DBSCANClustering(self, plot_graph=True, eps=0.1, min_samples=5):
+        # 2. Execute clustering (DBSCAN)
+        # eps=Neighborhood: distance radio to search neighbor points
+        # A core point is a point which has at least 'minPts' points in its neighborhood
+        # A border point is a point which has less than minPts in its neighborhood but
+        # it has a core point in its neighborhood
+        # A noise point doesn't have a core point in its neighborhood neither minPts points in its neighborhood
+        labels = DBSCAN(eps=eps, min_samples=min_samples).\
+                                        fit_predict(self.reduced_data)
+                                        
+        if plot_graph:
+            plt.scatter(self.reduced_data[:,0], self.reduced_data[:,1], c=labels)
+            plt.show()
     
     def PlotSilhouettes(self):
         plt.plot(self.silhouettes, 'bx-')
--- src/visualization/visualize_dataset.py
+++ src/visualization/visualize_dataset.py
@@ -1,10 +1,9 @@
 from common_clustering import CommonClustering
 
-#clustering_features = CommonClustering(r'C:\Users\ivangarrera\Desktop\T2_cleaned.csv')
-clustering_features = CommonClustering('D:\Ing. Informatica\Cuarto\Machine Learning\T2_cleaned_magneticField.csv')
+clustering_features = CommonClustering(r'C:\Users\ivangarrera\Desktop\T2_cleaned.csv')
+#clustering_features = CommonClustering('D:\Ing. Informatica\Cuarto\Machine Learning\T2_cleaned_magneticField.csv')
 
 clustering_features.PrincipalComponentAnalysis(num_components=2)
-clustering_features.PlotPCAResult()
 
 # Get the number of clusters that provides the best results
 ideal_number_of_clusters = clustering_features.getBestNumberOfClusters()
@@ -15,45 +14,9 @@ clustering_features.PlotSilhouettes()
 # Print k-means with the best number of clusters that have been found
 clustering_features.KMeansWithIdeal(ideal_number_of_clusters)
 
-def plotdata(data,labels,name): #def function plotdata
-#colors = ['black']
-    fig, ax = plt.subplots()
-    plt.scatter(data[:,0], data[:,1], c=labels)
-    ax.grid(True)
-    fig.tight_layout()
-    plt.title(name)
-    plt.show()
+# Agglomerative clustering algorithm using nearest neighbors matrix
+clustering_features.AgglomerativeClusteringWithNearestNeighbors()
 
-
-# 1. setting parameters
-# 1.1 Compute the similarity/distance matrix (high cost)
-# The graphic could offer better results (improve it!!)
-import matplotlib.pyplot as plt
-import sklearn.neighbors
-
-dist = sklearn.neighbors.DistanceMetric.get_metric('euclidean')
-matsim = dist.pairwise(clustering_features.reduced_data)
-
-# 1.2 Compute the k-nearest neighboors
-minPts=5
-from sklearn.neighbors import kneighbors_graph
-A = kneighbors_graph(clustering_features.reduced_data, minPts, include_self=False)
-Ar = A.toarray()
-
-seq = []
-for i,s in enumerate(clustering_features.reduced_data):
-    for j in range(len(clustering_features.reduced_data)):
-        if Ar[i][j] != 0:
-            seq.append(matsim[i][j])
-            
-seq.sort()
-plt.plot(seq)
-plt.show()
-
-# 2. Execute clustering (dbscan)
-import sklearn.cluster
-labels = sklearn.cluster.DBSCAN(eps=0.08, min_samples=minPts).fit_predict(clustering_features.reduced_data)
-
-# 3. Plot the results
-plotdata(clustering_features.reduced_data,labels, 'dbscan')
+# DBSCAN Clustering algorithm
+clustering_features.DBSCANClustering()
 
